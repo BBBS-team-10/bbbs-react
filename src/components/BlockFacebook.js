fdfafae51b92f0407359490fbf2d8e4f69ef8a9a
Zh <gomboevz@gmail.com>
2021-06-07 23:01:13 +0300

    fix: facebook iframe

--- src/components/BlockFacebook.js
+++ src/components/BlockFacebook.js
@@ -5,9 +5,9 @@ function BlockFacebook() {
     <div className="card card_color_blue card_content_media">
       <iframe
         title="facebook"
-        src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ffacebook&tabs=timeline&width=320&height=551&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
-        width="320"
-        height="551"
+        src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ffacebook&tabs=timeline&width=420&height=627&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+        width="420"
+        height="627"
         style={{ border: 'none', overflow: 'hidden' }}
         scrolling="no"
         frameBorder="0"
